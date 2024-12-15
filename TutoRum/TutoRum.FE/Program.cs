27838196c60a52bf8a88ec2e3d8d13c524a32381
lan.nguyen <lannthe160821@fpt.edu.vn>
2024-12-14 21:09:26 -0500

    no message

--- TutoRum/TutoRum.FE/Program.cs
+++ TutoRum/TutoRum.FE/Program.cs
@@ -34,7 +34,7 @@ builder.Services.AddSwaggerGen(option =>
 {
     option.AddServer(new OpenApiServer
     {
-        Url = "https://tutorconnectapi-d8gafsgrdka9gkbs.southafricanorth-01.azurewebsites.net"
+        Url = "https://tutorrumapi-abd8a3h4cnbafda8.southeastasia-01.azurewebsites.net"
     });
     option.SwaggerDoc("v1", new OpenApiInfo { Title = "TutorConnect API", Version = "v1" });
     option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
